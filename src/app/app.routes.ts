034fe9a41fd58ce2fba6438982f4f7da30f91ed1
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2024-05-11 03:44:08 +0500

    add 404 page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { MentorComponent } from './mentors/mentor/mentor.component';
 import { LoginComponent } from './core/auth/components/login/login.component';
 import { SignUpComponent } from './core/auth/components/sign-up/sign-up.component';
 import { AlertComponent } from './core/auth/components/alert/alert.component';
+import { NotFoundComponent } from './shared/components/not-found/notFound.component';
 
 export const routes: Routes = [
     {path:'admin', component: AdminComponent},
@@ -13,4 +14,6 @@ export const routes: Routes = [
     {path:'login', component: LoginComponent},
     {path:'sign-up', component: SignUpComponent},
     {path:'alert', component: AlertComponent},
+    {path:'not-found', component: NotFoundComponent},
+    {path:'**', redirectTo:'not-found'}
 ];
---
+++ src/app/shared/components/not-found/notFound.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'not-found',
+    templateUrl: 'notFound.component.html',
+    styleUrl:'notFound.component.scss',
+    standalone:true
+})
+
+export class NotFoundComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
